834f1a1498e2bc699c51b976597665cba2a7ed0a
Kauã Landi <kaualandi@hotmail.com>
2022-06-18 11:20:52 -0300

    add new port for server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
 server.use(router);
