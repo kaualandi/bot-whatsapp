588711454d84d570f0f8b8b3f47c65b23988f42c
Kauã Landi <eu@kaualf.com>
2023-01-03 21:21:29 -0300

    feat: add thanks message after refill

--- src/modules/setCredits.ts
+++ src/modules/setCredits.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from "@open-wa/wa-automate";
+import { ChatId, Client, Message } from "@open-wa/wa-automate";
 import { addClient, getClients, alterClient} from "../service/query";
 
 async function setCredits(client: Client, message: Message) {
@@ -8,19 +8,22 @@ async function setCredits(client: Client, message: Message) {
   const args = commands.split(" ");
 
   if (from !== '5521999222644@c.us') {
-    return client.reply(
+    await client.reply(
       from,
       "Você não tem permissão para executar esse comando!",
       id
     );
+    return;
   }
 
-  if (args.length < 3)
-    return client.reply(
+  if (args.length < 3) {
+    await client.reply(
       from,
       "Ainda não adivinho coisas... preciso saber o número e a quantidade de créditos também",
       id
     );
+    return;
+  }
 
   const phone = args[1];
   const credits = parseInt(args[2]) || 0;
@@ -29,11 +32,12 @@ async function setCredits(client: Client, message: Message) {
 
   const clients = await getClients();
   if (clients.status === "error") {
-    return client.reply(
+    await client.reply(
       from,
       `Não consegui recuperar os clientes!\n${clients.message.text}`,
       id
     );
+    return;
   }
 
   const _client = clients.find((client: any) => client.phone === `${phone}@c.us`);
@@ -42,33 +46,39 @@ async function setCredits(client: Client, message: Message) {
     const response = await addClient(credits, `${phone}@c.us`);
 
     if (response.status === "error") {
-      return client.reply(
+      await client.reply(
         from,
         `Não consegui criar o cliente!\n${response.message.text}`,
         id
       );
+      return;
     } else {
-      return client.reply(
+      await client.reply(
         from,
         `Agora ${phone} tem ${credits} creditos!`,
         id
       );
+      return;
     }
   } else {
     const response = await alterClient(credits, "credits", _client.id);
 
     if (response.status === "error") {
-      return client.reply(
+      await client.reply(
         from,
         `Não consegui editar o cliente!\n${response.message.text}`,
         id
       );
+      return;
     } else {
-      return client.reply(
+      const to = `${phone}@c.us`;
+      await client.sendText(to as ChatId, `*OBRIGADO POR RECARREGAR!*\nAgora você tem *${credits} créditos* para usar em qualquer consulta, caso tenha dúvidas de como utilizar digite _!menu consultas_ que te explico.`);
+      await client.reply(
         from,
         `Agora ${phone} tem ${credits} creditos!`,
         id
       );
+      return;
     }
   }
 }
